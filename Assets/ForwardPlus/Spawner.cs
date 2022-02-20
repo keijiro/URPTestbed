ebc6fc6d67b14b743ed089b4d39567066c9baa35
Keijiro Takahashi <keijiro@gmail.com>
2022-02-20 14:27:54 +0900

    Coloring

--- Assets/ForwardPlus/Spawner.cs
+++ Assets/ForwardPlus/Spawner.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
+using Random = Unity.Mathematics.Random;
 
 namespace UrpTestbed.ForwardPlus {
 
 sealed class Spawner : MonoBehaviour
 {
     [SerializeField] GameObject _template = null;
+    [SerializeField] Gradient _gradient = null;
 
     void Start()
     {
+        var rand = new Random(100);
+
         for (var s = 0; s < 8; s++)
         {
             for (var t = 0; t < 4; t++)
@@ -17,7 +21,7 @@ sealed class Spawner : MonoBehaviour
 
                 var go = Instantiate(_template, pos, Quaternion.identity);
 
-                var color = Color.HSVToRGB(Random.value, 1, 1);;
+                var color = _gradient.Evaluate(rand.NextFloat());
                 go.GetComponentInChildren<Light>().color = color;
                 go.GetComponentInChildren<Renderer>().material.color = color * 1.5f;
             }
